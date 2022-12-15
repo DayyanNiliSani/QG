dc7dea34aa1e8677306c66dc2a34167d8152b19d
d.nilisani <d.nilisani@mrbilit.com>
2022-12-15 22:17:53 +0330

    added swagger and logger

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,29 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      logger: true,
+    }),
   );
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('QG')
+    .setDescription('A feature rich quiz game')
+    .setVersion('1.0')
+    .addTag('qg')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 
